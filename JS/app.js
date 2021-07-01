a829ab491a0e9ad85875205f444615dd208fb55b
abdullahnazzal <a.nazzal1995@gmail.com>
2021-07-01 18:36:59 +0300

    add pizza shop

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+// 'use strict' 
+
+// let myFormEl = document.getElementById("myForm");
+// myFormEl.addEventListener("submit", addP);
+// let list=[];
+
+// function addP(event) {
+//     event.preventDefault();
+//     let list=[];
+//     let cupsmilk = event.target.cupsmilk.checked;
+//     let cupmascarpone = event.target.cupmascarpone.checked;
+//     let pinksalt = event.target.pinksalt.checked;
+//     let BlackMissionFigs = event.target.BlackMissionFigs.checked;
+//     let brownSugar = event.target.brownSugar.checked;
+//     let tbspWater = event.target.tbspWater.checked;
+//     let cupsHeavyCream = event.target.cupsHeavyCream.checked;
+//     let granulatedSugar = event.target.granulatedSugar.checked;
+//     let eggYolks = event.target.eggYolks.checked;
+//     let lemonJuiced = event.target.lemonJuiced.checked;
+//     let tbspButter = event.target.tbspButter.checked;
+//     let honeyRoasted = event.target.honeyRoasted.checked;
+//     for (let i = 0; i < 13; i++) {
+//         list.push(cupsmilk);
+//     }
+
+// }
